d89ab03b6dfc8c36e0374de7e3ac1d1d8bfa7a4f
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 13:19:41 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program asks the user for a letter grade and will ouput what percentage range that would be
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // import scanner for user inputs
+    Scanner input = new Scanner(System.in);
+
+    // ask user to enter a letter grade
+    System.out.println("Please enter a letter grade:");
+    String letterGrade = input.nextLine();
+
+    switch ( letterGrade ) {
+      case "A":
+        System.out.println("This is between 80% and 100%");
+        break;
+      case "B":
+        System.out.println("This is between 70% and 79%");
+        break;
+      case "C":
+        System.out.println("This is between 60% and 69%");
+        break;
+      case "D":
+        System.out.println("This is between 50% and 59%");
+        break;
+      case "F":
+        System.out.println("This is between 0% and 49%");
+        break;
+      
+
+    }    
   }
 }
